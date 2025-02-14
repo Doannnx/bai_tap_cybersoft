abcd1a74f3a4e23f30891393b93841474fcd665a
Doannnx <doanhamhoc@gmail.com>
2025-02-14 16:23:43 +0700

    add tinh tong so le

---
+++ vongLap/tinhTongSoLe.java
@@ -0,0 +1,30 @@
+package vongLap;
+import java.util.Scanner;
+
+public class tinhTongSoLe {
+
+	public tinhTongSoLe() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int num ;
+		int sum = 0;
+		Scanner nhap= new Scanner(System.in);
+		
+		System.out.println("Nhập số nguyên lớn hơn 0:");
+		num = Integer.parseInt(nhap.nextLine());
+		
+		if (num >0) {
+			for (int i = 1; i <= num; i+=2) {
+				sum += i;
+			}
+		}else {
+			System.out.println("Vui lòng nhập lại");
+		}
+		
+		System.out.println("Tổng các số bé hơn " +num+" là: " +sum);
+
+	}
+
+}
